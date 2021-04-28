00cbdc2ed8cffa1bc3de0b20aa4073604d829a26
Lucas Keterson <lucasketersonsk8@gmail.com>
2021-04-28 11:43:08 -0300

    Sound FX
    Button Sound FX BNB/UI
    Graphs Axis Name Added

---
+++ LeapLearnV4.01 - Unity 2019/Assets/ProjectFiles/Scripts/SoundPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+
+    public AudioSource buttonSound;
+    // Start is called before the first frame update
+  
+    public void PlayButtonSound()
+    {
+        buttonSound.Play();
+    }
+}
